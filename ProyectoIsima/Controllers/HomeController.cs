d022b580daea268f532b597316eb863234f30f67
Kevin Mejia <kevin_me_ga@hotmail.com>
2015-05-19 14:31:40 -0500

    Primer commit del Proyecto

---
+++ ProyectoIsima/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoIsima.Models;
+
+namespace ProyectoIsima.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private ProyectoIsimaDB db = new ProyectoIsimaDB();
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View(db.Alumno.ToList());
+        }
+
+    }
+}
---
+++ ProyectoIsima/Models/Alumnos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ProyectoIsima.Models
+{
+    public class Alumnos
+    {
+        [Key]
+        public int Id_Alumno { get; set; }
+        [Required(ErrorMessage="Ingrese un nombre de alumno")]
+        public string NombreAlumno { get; set; }
+        [Required(ErrorMessage="Ingrese el apellido paterno del alumno")]
+        public string ApellidpPatAlumno { get; set; }
+        [Required(ErrorMessage="Ingrese el apellido materno del alumno")]
+        public string ApellidoMatAlumno { get; set; }
+    }
+
+    public class ProyectoIsimaDB : DbContext
+    {
+        public DbSet<Alumnos> Alumno { get; set; }
+    }
+}
